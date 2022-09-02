cf5b92b3d8e54c68326ee32b5fc513cec09d5a78
Evgenij <evgenij_eliseew@live.de>
2022-09-02 17:59:23 +0200

    import args

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from re import A
 import time
-from sys import argv
+import argparse
 import numpy as np
 from scipy.stats import norm
 from joblib import Parallel, delayed
 
 UOM = '€'
 
+parser = argparse.ArgumentParser(description='monte-carlo калькулятор')
+parser.add_argument("-f", "--first_deposit", required=True, type=int, help="Первый взнос")
+parser.add_argument("-m", "--monthly_deposit", required=True, type=int, help="Ежемесячный взнос")
+parser.add_argument("-%", "--interest_rate", required=True, type=float, help="Процентная ставка")
+parser.add_argument("-v", "--volatility", required=True, type=int, help="Волатильность")
+parser.add_argument("-d", "--dynamic", required=True, type=int, help="Динамика")
+parser.add_argument("-y", "--years_number", required=True, type=int, help="Срок инвестирования (лет)")
+parser.add_argument("-i", "--iter_number", required=True, type=int, help="Количество итераций")
+
+args = parser.parse_args()
 
 def calc_ending_value(monthly_deposit, interest_rate, volatility, dynamic, ending_value):
 
@@ -63,28 +73,29 @@ def calc(first_deposit, monthly_deposit, interest_rate, volatility, dynamic, yea
 if __name__ == '__main__':
     start_time = time.time()
 
-    first_deposit = float(input("Первый взнос "))
-    monthly_deposit = float(input("Ежемесячный взнос "))
-    interest_rate = float(input("Процентная ставка "))
-    interest_rate = interest_rate / 100.
-    volatility = float(input("Волатильность "))
-    volatility = volatility / 100.
-    dynamic = float(input("Динамика "))
-    dynamic = dynamic / 100.
-    years_number = int(input("Лет "))
-    iter_number = int(input("Количество итераций метода Монте-Карло: "))
+first_deposit = args.first_deposit
+monthly_deposit = args.monthly_deposit
+interest_rate = args.interest_rate
+interest_rate = interest_rate / 100
+volatility = args.volatility
+volatility = volatility / 100
+dynamic = args.dynamic
+dynamic = dynamic / 100
+years_number = args.years_number
+iter_number = args.iter_number
 
-    percentiles = [99, 95, 75, 25, 5, 1]
-    novolatility_value, mean, median, percentile_vals = calc(first_deposit, monthly_deposit, interest_rate, volatility, dynamic, years_number, iter_number, percentiles)
 
-    print(f'[+] Нулевая волатильность: ')
-    print(f'Значение: {novolatility_value:,.0f} {UOM}')
-    print()
-    print('[+] Метод Монте-Карло:')
-    print(f'Среднее значение: {mean:,.0f} {UOM}')
-    print(f'Медиана: {median:,.0f} {UOM}')
-    for i, perc in enumerate(percentile_vals[0]):
-        print(f'Процентиль {percentiles[i]}%: {perc:,.0f} {UOM}')
-    print()
+percentiles = [99, 95, 75, 25, 5, 1]
+novolatility_value, mean, median, percentile_vals = calc(first_deposit, monthly_deposit, interest_rate, volatility, dynamic, years_number, iter_number, percentiles)
 
-    print("Время счета: %s секунд" % (time.time() - start_time))
+print(f'[+] Нулевая волатильность: ')
+print(f'Значение: {novolatility_value:,.0f} {UOM}')
+print()
+print('[+] Метод Монте-Карло:')
+print(f'Среднее значение: {mean:,.0f} {UOM}')
+print(f'Медиана: {median:,.0f} {UOM}')
+for i, perc in enumerate(percentile_vals[0]):
+    print(f'Процентиль {percentiles[i]}%: {perc:,.0f} {UOM}')
+print()
+
+print("Время счета: %s секунд" % (time.time() - start_time))
